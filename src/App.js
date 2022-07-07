76b8324ec072cf1384c9a1793f2f42f610a111be
Zero to Mastery <vkormoczy@gmail.com>
2022-07-07 22:50:54 +0300

    Add routers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Categories from "./components/Categories";
 
-function App() {
-  return <div className="App" />;
-}
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/books" element={<BooksContainer />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
