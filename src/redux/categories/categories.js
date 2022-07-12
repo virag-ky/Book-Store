d288eb3b6857e9970b0568d97279888d3d220b96
Virag Kormoczy <vkormoczy@gmail.com>
2022-07-12 19:44:22 +0300

    Add categories functionality

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,6 +1,6 @@
 const CHECK_STATUS = 'CHECK_STATUS';
 
-const categoriesReducer = (state = [], action) => {
+const categoriesReducer = (state = '', action) => {
   switch (action.type) {
     case CHECK_STATUS:
       return 'Under construction';
