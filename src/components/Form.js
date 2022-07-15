24b67479096db2df341275dac541af06a6e1cd18
Virag Kormoczy <vkormoczy@gmail.com>
2022-07-15 18:44:16 +0300

    Add autocomplete for the select element

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,42 +1,35 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { useTheme } from '@mui/material/styles';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
 import { v4 as uuidv4 } from 'uuid';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 import { addNewBook } from '../redux/books/books';
+
+const categories = [
+  'Category',
+  'Action',
+  'Adventure',
+  'Fantasy',
+  'Horror',
+  'Classic',
+  'History',
+  'Detective and Mystery',
+  'Romance',
+  'Sci-Fi',
+  'Humor',
+  'Tragedy',
+  'Computer Programming',
+  'Documentary',
+];
 
 const Form = () => {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
-  const [categoryName, setCategoryName] = useState([]);
-  const theme = useTheme();
+  const [categoryName, setCategoryName] = useState(categories[0]);
+  const [inputValue, setInputValue] = useState('');
   const dispatch = useDispatch();
 
   const id = uuidv4();
-
-  const ITEM_HEIGHT = 48;
-  const ITEM_PADDING_TOP = 8;
-  const MenuProps = {
-    PaperProps: {
-      style: {
-        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-        width: 250,
-      },
-    },
-  };
-
-  function getStyles(category, categoryName, theme) {
-    return {
-      fontWeight:
-        categoryName.indexOf(category) === -1
-          ? theme.typography.fontWeightRegular
-          : theme.typography.fontWeightMedium,
-    };
-  }
 
   const addBookToList = () => {
     dispatch(
@@ -44,7 +37,7 @@ const Form = () => {
         title,
         author,
         id,
-        category: categoryName.join(''),
+        category: categoryName,
       }),
     );
     setTitle('');
@@ -52,38 +45,12 @@ const Form = () => {
     setCategoryName([]);
   };
 
-  const categories = [
-    'Action',
-    'Adventure',
-    'Fantasy',
-    'Horror',
-    'Classic',
-    'History',
-    'Detective and Mystery',
-    'Romance',
-    'Sci-Fi',
-    'Humor',
-    'Tragedy',
-    'Computer Programming',
-    'Documentary',
-  ];
-
   const changeTitle = (e) => {
     setTitle(e.target.value);
   };
 
   const changeAuthor = (e) => {
     setAuthor(e.target.value);
-  };
-
-  const handleChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    setCategoryName(
-      // On autofill we get a stringified value.
-      typeof value === 'string' ? value.split(',') : value,
-    );
   };
 
   return (
@@ -104,28 +71,22 @@ const Form = () => {
         onChange={changeAuthor}
       />
       <div>
-        <FormControl sx={{ m: 1, width: 300 }}>
-          <InputLabel id="demo-multiple-name-label">Category</InputLabel>
-          <Select
-            labelId="demo-multiple-name-label"
-            id="demo-multiple-name"
-            multiple
-            value={categoryName}
-            onChange={handleChange}
-            input={<OutlinedInput label="Category" />}
-            MenuProps={MenuProps}
-          >
-            {categories.map((category) => (
-              <MenuItem
-                key={category}
-                value={category}
-                style={getStyles(category, categoryName, theme)}
-              >
-                {category}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+        <Autocomplete
+          value={categoryName}
+          onChange={(event, newValue) => {
+            setCategoryName(newValue);
+          }}
+          inputValue={inputValue}
+          onInputChange={(event, newInputValue) => {
+            setInputValue(newInputValue);
+          }}
+          id="controllable-states-demo"
+          options={categories}
+          sx={{ width: 300 }}
+          /* eslint-disable */
+          renderInput={(params) => <TextField {...params} />}
+          /* eslint-enable */
+        />
       </div>
       <button type="button" onClick={addBookToList}>
         Add Book
